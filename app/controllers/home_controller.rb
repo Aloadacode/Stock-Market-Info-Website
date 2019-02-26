66fe163990b900bbea4a447e4c629c873abf34e9
George Pearson <george.pearson@hotmail.com>
2019-02-26 21:45:05 +0000

    added everything up to the account feature, works fine

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,23 @@
 class HomeController < ApplicationController
   def index
   
+    if params[:id] == ""
+      @nothing = "You failed. Please enter a company ticker symbol"
+    elsif 
+      
+      if params[:id]
+        begin 
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue StandardError
+          @error = "You must be dreaming, that stock doesn't exist....please try again."
+        end
+      end 
+    end
+    
+    
+  end  
+  
   def about
   end
   
-  end
 end
